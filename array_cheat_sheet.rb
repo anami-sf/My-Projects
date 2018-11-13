1cbdab626271051f86de658c73fc81f2c349c325
Ana <anami823@gmail.com>
2018-11-13 10:14:47 -0800

    First commit

---
+++ array_cheat_sheet.rb
@@ -0,0 +1,45 @@
+a = [2, 3, 'sun',  8]
+b = [1, 2, 8, 'moon', 11]
+c = [5, 7, 2, 5 ,11, 5,]
+d = ['fish', 'dog', 'cat']
+g = []
+h = [2, 3, 5]
+j = [1,0,2]
+k = 
+l = nil
+
+
+
+
+=begin
+print "#{a}\n + \n #{b} n \==> #{a + b}"
+puts
+print 
+print "#{a} | #{b} ==> #{a | b}" 
+puts
+print "#{a} & #{b} ==> #{a & b}"
+puts
+print "#{a} - #{b} ==> #{a - b}"
+puts 
+print "#{b} * 3 ==> #{a + b}"
+puts 
+puts "b.length ==> #{b.length}"
+print c.sort
+puts
+print b.uniq
+puts
+print b.delete_at(3)
+puts
+print b
+puts
+print c.join
+puts
+print d.join(', ')
+puts
+puts "b.length ==> #{b.length}"
+puts "b.size ==> #{b.size}"
+g.push(3, 6, 7)
+print g
+puts
+=end
+
